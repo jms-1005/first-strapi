5af7d9513a9b00ef93bae69f9fdb74e73ccff4ca
John Manoah <john@vanarts.com>
2022-07-21 11:01:31 -0700

    added ckeditor and a new content type

---
+++ src/api/newcontenttype/controllers/newcontenttype.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  newcontenttype controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::newcontenttype.newcontenttype');
---
+++ src/api/newcontenttype/services/newcontenttype.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newcontenttype service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::newcontenttype.newcontenttype');
